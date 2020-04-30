99dba2b3ca2f3fc24ba6c0537aa04d1f7df6f1c8
huiweiyong <307468262@qq.com>
2020-04-30 16:48:18 +0800

    完成C++基础框架

---
+++ flash/Src/main.cpp
@@ -0,0 +1,32 @@
+
+#include "stm32f10x.h"
+
+
+class xyos
+{
+private:
+    /* data */
+public:
+    //xyos();
+    //  xyos();
+     void loop(void);
+};
+
+
+void xyos::loop(void)
+{
+
+}
+
+xyos   os;
+
+
+int main(void)
+{
+    while (1)
+    {
+        /* code */
+        os.loop();
+    }
+    
+}
